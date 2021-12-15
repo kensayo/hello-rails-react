1ae1a6c33e6ea8f1957dc763621c1535b72a2f4f
Kenny Ortega <kennyaoc@gmail.com>
2021-12-14 23:10:47 -0400

    Update README and Linters

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -1,8 +1,11 @@
-class V1::MessagesController < ApplicationController
-  def index
-    message = Message.order(Arel.sql('RANDOM()')).first.message
-    # message = Message.all.pluck(:message)
-    render json: { :message => message  }.to_json
+# frozen_string_literal: true
 
+module V1
+  class MessagesController < ApplicationController
+    def index
+      message = Message.order(Arel.sql('RANDOM()')).first.message
+      # message = Message.all.pluck(:message)
+      render json: { message: message }.to_json
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  namespace :v1, defaults: {format: 'json'} do
+  namespace :v1, defaults: { format: 'json' } do
     get 'message', to: 'messages#index'
   end
-  get '*page', to: 'statics#index', constraints: ->(req) do
+  get '*page', to: 'statics#index', constraints: lambda { |req|
     !req.xhr? && req.format.html?
-  end
+  }
   root 'statics#index'
 end
