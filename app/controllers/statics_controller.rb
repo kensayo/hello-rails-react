12f4e8996cb2921672cef8206a38d2ccb99846ee
Kenny Ortega <kennyaoc@gmail.com>
2021-12-14 23:28:05 -0400

    Fix linters

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class StaticsController < ApplicationController
   def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     get 'message', to: 'messages#index'
